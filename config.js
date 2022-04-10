552b61378b14f655d301bbfb2bb4c0655d934670
Wibugans <95990403+Wibugans@users.noreply.github.com>
2022-04-10 19:44:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-global.owner = ['6287892711054'] // Put your number here
+global.owner = ['6281275274328'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
@@ -44,7 +44,7 @@ global.APIKeys = { // APIKey Here
 global.stiker_wait = 'Stiker sedang dibuat'
 global.packname = 'stickerby'
 global.author = `𝚂𝙷𝙸𝚁𝙾-𝙼𝙳`
-global.wm = '𝙷𝚈𝚉𝙴𝚁 𝙾𝙵𝙵𝙸𝙲𝙸𝙰𝙻'
+global.wm = 'Yusril official'
 global.watermark = wm
 global.media = 'https://telegra.ph/file/72168c6f1aaa90315c369.jpg'
 global.instagram = 'https://instagram.com/hyzer-stfu'
